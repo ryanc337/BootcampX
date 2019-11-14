062a5decefa6be6298aba024b3500cde8e6db020
Ryan <cordingley.ryan@gmail.com>
2019-11-14 04:02:37 +0000

    added student.js file and teachers.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  database: 'bootcampx'
+});
+
+let vars = process.argv.slice(2);
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;`
+).then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort} : ${user.teacher}`);
+  })
+}).catch(err => console.error('query-error', err.stack));
